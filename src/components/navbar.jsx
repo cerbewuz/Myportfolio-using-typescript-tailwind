85694183e569427bf144fcb9037d1e09c7ba8fbb
Renvic <r.emenido03@gmail.com>
2024-11-07 00:09:54 +0800

    update of navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,34 +6,34 @@ const Navbar = () => {
       {/* Navbar container */}
       <div className="absolute top-0 left-0 w-full bg-transparent">
         {/* Navbar content */}
-        <div className="flex justify-center items-center p-4 md:p-6">
+        <div className="flex justify-center items-center px-4 py-2 md:px-6 md:py-4">
 
-          {/* Navbar Links */}
-          <div className="flex space-x-6 items-center text-black">
+           {/* Navbar Links */}
+           <div className="flex space-x-4 sm:space-x-6 md:space-x-8 items-center text-black">
             <a
               href="#"
-              className="group relative p-3 cursor-pointer text-lg font-kanit transition-all duration-300 ease-in-out"
+              className="group relative p-2 sm:p-3 cursor-pointer text-sm sm:text-lg md:text-xl font-kanit transition-all duration-300 ease-in-out"
             >
               Home
               <span className="absolute left-0 bottom-0 w-full h-1 bg-transparent group-hover:bg-gray-700 transition-all duration-300"></span>
             </a>
             <a
               href="#"
-              className="group relative p-3 cursor-pointer text-lg font-kanit transition-all duration-300 ease-in-out"
+              className="group relative p-2 sm:p-3 cursor-pointer text-sm sm:text-lg md:text-xl font-kanit transition-all duration-300 ease-in-out"
             >
               Skills
               <span className="absolute left-0 bottom-0 w-full h-1 bg-transparent group-hover:bg-gray-700 transition-all duration-300"></span>
             </a>
             <a
               href="#"
-              className="group relative p-3 cursor-pointer text-lg font-kanit transition-all duration-300 ease-in-out"
+              className="group relative p-2 sm:p-3 cursor-pointer text-sm sm:text-lg md:text-xl font-kanit transition-all duration-300 ease-in-out"
             >
               Projects
               <span className="absolute left-0 bottom-0 w-full h-1 bg-transparent group-hover:bg-gray-700 transition-all duration-300"></span>
             </a>
             <a
               href="#"
-              className="group relative p-3 cursor-pointer text-lg font-kanit transition-all duration-300 ease-in-out"
+              className="group relative p-2 sm:p-3 cursor-pointer text-sm sm:text-lg md:text-xl font-kanit transition-all duration-300 ease-in-out"
             >
               Contact Me
               <span className="absolute left-0 bottom-0 w-full h-1 bg-transparent group-hover:bg-gray-700 transition-all duration-300"></span>
